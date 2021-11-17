59b95ddbdb6ec387340bd8dac900b3ba80955717
jializhong <jializhong@saimo.ai>
2021-11-17 18:06:18 +0800

    init

---
+++ src/main/java/com/jlz/security/demo/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.jlz.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author jlz
+ * @className: SecurityConfig
+ * @date 2021/11/17 15:38
+ * @description todo
+ **/
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //不加密 已过期
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //基于内存的用户密码
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("123456")
+                .roles("admin")
+                .and()
+                .withUser("jlz")
+                .password("123456")
+                .roles("admin");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //自定义表单登录
+        http
+                .formLogin()
+                .loginPage("/login.html")
+                //登录页放行
+                .permitAll()
+        .and()
+        .authorizeRequests()
+                //所有请求都要经过验证
+                .anyRequest()
+                .authenticated()
+                //设置自定义登录页
+                .and()
+                .csrf()
+                .disable()
+        ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //放心静态资源文件
+        web.ignoring().antMatchers("/js/**", "/css/**","/images/**");
+    }
+}
---
+++ src/main/java/com/jlz/security/demo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.jlz.security.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jlz
+ * @className: HelloController
+ * @date 2021/11/17 11:37
+ * @description todo
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
